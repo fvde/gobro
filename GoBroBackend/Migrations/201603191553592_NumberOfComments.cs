0e35447fd063e20429d5ceb77ec1cb57f2b683d6
Julian Sievers <sieversjulian@gmail.com>
2016-03-19 17:22:37 +0100

    -  added number of comments migration

---
+++ GoBroBackend/Migrations/201603191553592_NumberOfComments.cs
@@ -0,0 +1,16 @@
+namespace GoBroBackend.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class NumberOfComments : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
